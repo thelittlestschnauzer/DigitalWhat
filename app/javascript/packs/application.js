9434311a918f2b814601c053c1b4060b834aec1a
github username <doneil1580@gmail.com>
2019-10-02 21:57:53 -0400

    added bootstrap through webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("materialize")
 
+import './bootstrap_custom.js'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
